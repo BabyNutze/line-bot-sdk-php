ca43c6fe168517ba854b90a50f57f304e182f488
BabyNutze <babysparker@live.com>
2019-05-03 07:58:07 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,10 @@
 <?php
-$conn = new mysqli("localhost", "root","1234","linebot");
-/* check connection */
+
+$db = parse_url(getenv("postgres://qlalwjlsbccpqs:633bc9695e4fb4a66bdd23140ea8f4f10f10056b24d2297b83b591cc6e932a08@ec2-54-83-205-27.compute-1.amazonaws.com:5432/da3pmg91gstk4k"));
+$db["path"] = ltrim($db["path"], "/");
+
+/*--$conn = new mysqli("localhost", "root","1234","linebot");
+
 if ($conn->connect_errno) {
     printf("Connect failed: %s\n", $conn->connect_error);
     exit();
@@ -9,4 +13,5 @@ if(!$conn->set_charset("utf8mb4")) {
     printf("Error loading character set utf8: %s\n", $conn->error);
     exit();
 }
+*/
 ?>
